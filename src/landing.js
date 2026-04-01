7f5a40fd2760dd5b0b76ee0f1b1fc0f3eb19301b
Awanit Kumar Singh <awanitsingh8873@gmail.com>
2026-04-01 23:51:20 +0530

    Remove unused Logo import from landing

--- src/landing.js
+++ src/landing.js
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Logo from "./components/Logo";
 
 const CROPS = ["Rice", "Wheat", "Maize", "Cotton", "Mango", "Sugarcane"];
 
